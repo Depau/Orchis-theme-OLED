75a14f13deb53a372c84d706a7a1c1f75f6ff22a
Davide Depau <davide@depau.eu>
2021-03-08 19:45:04 +0100

    Fix asset colors

--- src/gtk/render-assets.sh
+++ src/gtk/render-assets.sh
@@ -9,42 +9,50 @@ INDEX="assets.txt"
 
 for theme in '' '-purple' '-pink' '-red' '-orange' '-yellow' '-green' '-grey'; do
 
-ASSETS_DIR="assets${theme}"
-SRC_FILE="assets${theme}.svg"
+  ASSETS_DIR="assets${theme}"
+  SRC_FILE="assets${theme}.svg"
 
-[[ -d $ASSETS_DIR ]] && rm -rf $ASSETS_DIR
-mkdir -p $ASSETS_DIR
+  [[ -d $ASSETS_DIR ]] && rm -rf $ASSETS_DIR
+  mkdir -p $ASSETS_DIR
 
-for i in `cat $INDEX`; do
-echo "Rendering '$ASSETS_DIR/$i.png'"
-if [[ -n "${RENDER_SVG}" ]]; then
-  "$RENDER_SVG" --export-id "$i" \
-                "$SRC_FILE" "$ASSETS_DIR/$i.png"
-else
-  "$INKSCAPE" --export-id="$i" \
-              --export-id-only \
-              --export-filename="$ASSETS_DIR/$i.png" "$SRC_FILE" >/dev/null
-fi
-if [[ -n "${OPTIPNG}" ]]; then
-  "$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i.png"
-fi
+  for i in `cat $INDEX`; do
+    echo "Rendering '$ASSETS_DIR/$i.png'"
+    if [[ -n "${RENDER_SVG}" ]]; then
+      ("$RENDER_SVG" --export-id "$i" \
+                    "$SRC_FILE" "$ASSETS_DIR/$i.png") &
+    else
+      ("$INKSCAPE" --export-id="$i" \
+                  --export-id-only \
+                  --export-filename="$ASSETS_DIR/$i.png" "$SRC_FILE" >/dev/null) &
+    fi
 
-echo "Rendering '$ASSETS_DIR/$i@2.png'"
-if [[ -n "${RENDER_SVG}" ]]; then
-  # @TODO: remove --zoom when it will be fixed/implemented in resvg
-  "$RENDER_SVG" --export-id "$i" \
-                --dpi 192 \
-                --zoom 2 \
-                "$SRC_FILE" "$ASSETS_DIR/$i@2.png"
-else
-  "$INKSCAPE" --export-id="$i" \
-              --export-id-only \
-              --export-dpi=192 \
-              --export-filename="$ASSETS_DIR/$i@2.png" "$SRC_FILE" >/dev/null
-fi
-if [[ -n "${OPTIPNG}" ]]; then
-  "$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i@2.png"
-fi
+    echo "Rendering '$ASSETS_DIR/$i@2.png'"
+    if [[ -n "${RENDER_SVG}" ]]; then
+      # @TODO: remove --zoom when it will be fixed/implemented in resvg
+      ("$RENDER_SVG" --export-id "$i" \
+                    --dpi 192 \
+                    --zoom 2 \
+		    "$SRC_FILE" "$ASSETS_DIR/$i@2.png") &
+    else
+      ("$INKSCAPE" --export-id="$i" \
+                  --export-id-only \
+                  --export-dpi=192 \
+		  --export-filename="$ASSETS_DIR/$i@2.png" "$SRC_FILE" >/dev/null) &
+    fi
+  done
+
+  wait
+
+  for i in `cat $INDEX`; do
+    if [[ -n "${OPTIPNG}" ]]; then
+      ("$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i.png") &
+    fi
+
+    if [[ -n "${OPTIPNG}" ]]; then
+      ("$OPTIPNG" -o7 --quiet "$ASSETS_DIR/$i@2.png") &
+    fi
+  done
+
 done
 
-done
+wait
